4e34222e6b2dcca151971a336841396349d9898f
Camelia Groza <camelia.groza1606@yahoo.com>
2021-10-04 15:55:09 +0300

    commit

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+def get_largest_prime_below(n):
+    '''
+    Gaseste ultimul numar prim mai mic decat un numar dat
+    :param n: un numar natural
+    :return: numarul cerut
+    '''
+    for i in range (n-1,1,-1):
+        stop=0
+        for j in range (2,n//2+1):
+            if i%j==0:
+                stop=1
+        if stop==0:
+            return(i)
+
+def test_get_largest_prime_below():
+    '''
+    Functia de test
+    '''
+    assert get_largest_prime_below(20) == 19
+    assert get_largest_prime_below(3) == 2
+    assert get_largest_prime_below(100) == 97
+
+def main():
+    a=int(input("Dati numarul: "))
+    while a!=-1:
+        print (get_largest_prime_below(a))
+        a=int(input("Dati alt numar: "))
+
+if __name__ == '__main__':
+  main()
+
+
+
 def is_palindrome(n):
     '''
     Determina daca un numar dat este palindrom
@@ -24,39 +57,14 @@ def test_is_palindrome():
     assert is_palindrome(7887) == True
     assert is_palindrome(244) == False
 
-a=int(input("Dati numarul: "))
-while a!=-1:
-    print (is_palindrome(a))
-    a=int(input("Dati alt numar: "))
+def main():
+    a=int(input("Dati numarul: "))
+    while a!=-1:
+        print (is_palindrome(a))
+        a=int(input("Dati alt numar: "))
 
-
-
-def get_largest_prime_below(n):
-    '''
-    Gaseste ultimul numar prim mai mic decat un numar dat
-    :param n: un numar natural
-    :return: numarul cerut
-    '''
-    for i in range (n-1,1,-1):
-        stop=0
-        for j in range (2,n//2+1):
-            if i%j==0:
-                stop=1
-        if stop==0:
-            return(i)
-
-def test_get_largest_prime_below():
-    '''
-    Functia de test
-    '''
-    assert get_largest_prime_below(20) == 19
-    assert get_largest_prime_below(3) == 2
-    assert get_largest_prime_below(100) == 97
-
-a=int(input("Dati numarul: "))
-while a!=-1:
-    print (get_largest_prime_below(a))
-    a=int(input("Dati alt numar: "))
+if __name__ == '__main__':
+  main()
 
 
 
@@ -99,7 +107,11 @@ def test_is_superprime():
     assert is_superprime(237) == False
     assert is_superprime(3) == True
 
-a=int(input("Dati numarul: "))
-while a!=-1:
-    print (is_superprime(a))
-    a=int(input("Dati alt numar: "))
+def main():
+    a = int(input("Dati numarul: "))
+    while a != -1:
+        print(is_superprime(a))
+        a = int(input("Dati alt numar: "))
+
+if __name__ == '__main__':
+  main()
